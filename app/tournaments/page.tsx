b8d8adcdab3629b82a975101fc8d29f66f15fdc0
Luke Enness <lukeenness@outlook.com>
2026-07-18 20:24:47 +0100

    Added api routes that connect to the supabase and further adjusted the project layout

---
+++ app/tournaments/page.tsx
@@ -0,0 +1,11 @@
+import { Tournament } from "./Tournament";
+
+export default async function TournamentsPage() {
+    let tournaments: Tournament[] = [];
+
+    return (
+        <div className="flex-1 w-full min-h-screen bg-zinc-50 dark:bg-black py-12 px-6 sm:px-12 md:px-16">
+            <main className="max-w-6xl mx-auto flex flex-col gap-10"></main>
+        </div>
+    );
+}
